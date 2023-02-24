a39483c672b1714c7ebfdfae89f4b3fa851e3814
RaulMorales404 <r20160106@gmail.com>
2023-02-24 01:32:20 -0600

    signIn Gemail in android

---
+++ ReduxToolkidTypesc/src/screens/Authenticate.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
+
+interface Props {
+    handleGoogleButtonPress:any;
+}
+export default function Authenticate(props: Props) {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.title}>Google OAuth</Text>
+            <GoogleSigninButton onPress={props.handleGoogleButtonPress} />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    screen: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 25,
+        marginBottom: 30
+    }
+})
